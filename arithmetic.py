955ed71ce2d4846323c34d09121ded429220e74d
Hackbright Student <info@hackbrightacademy.com>
2016-07-07 11:48:02 -0700

    Added functions for square, cube, power and mod. Updated division function.

--- arithmetic.py
+++ arithmetic.py
@@ -8,16 +8,16 @@ def multiply(num1, num2):
     return num1 * num2
 
 def divide(num1, num2):
-    return num1 / num2
+    return num1 / float(num2)
 
 def square(num1):
-    pass
+    return num1 ** 2
 
 def cube(num1):
-    pass
+    return num1 ** 3
 
 def power(num1, num2):
-    pass
+    return num1 ** num2
 
 def mod(num1, num2):
-    pass
+    return num1 % num2
